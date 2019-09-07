8f78ff1bfe87f0ffc429ed0fc8e90db43a98fa86
kaba_clone <kabaclone@gmail.com>
2019-09-07 15:24:53 +0900

    change not request API

--- isucari/webapp/python/app.py
+++ isucari/webapp/python/app.py
@@ -621,7 +621,12 @@ def get_transactions():
                         if not shipping:
                             http_json_error(requests.codes['not_found'], "shipping not found")
 
-                        ssr = api_shipment_status(get_shipment_service_url(), {"reserve_id": shipping["reserve_id"]})
+                        if shipping["status"] == "initial":
+                            ssr = {"status": 'wait_shipping'}
+                        elif shipping["status"] == "done":
+                            ssr = {"status": 'done'}
+                        else:
+                            ssr = api_shipment_status(get_shipment_service_url(), {"reserve_id": shipping["reserve_id"]})
                         item["transaction_evidence_id"] = transaction_evidence["id"]
                         item["transaction_evidence_status"] = transaction_evidence["status"]
                         item["shipping_status"] = ssr["status"]
